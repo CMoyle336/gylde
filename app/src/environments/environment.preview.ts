5765009489720ddcea7094bbc890ff0e15db5442
Christopher Moyle <chmoyle@conga.com>
2026-01-23 10:56:46 -0500

    updated preview environment

--- app/src/environments/environment.preview.ts
+++ app/src/environments/environment.preview.ts
@@ -3,13 +3,13 @@ export const environment = {
   useEmulators: false,
   firebase: {
     // TODO: Update with your preview Firebase project config
-    apiKey: 'YOUR_PREVIEW_API_KEY',
-    authDomain: 'gylde-preview.firebaseapp.com',
-    projectId: 'gylde-preview',
-    storageBucket: 'gylde-preview.firebasestorage.app',
-    messagingSenderId: 'YOUR_PREVIEW_SENDER_ID',
-    appId: 'YOUR_PREVIEW_APP_ID',
-    measurementId: 'YOUR_PREVIEW_MEASUREMENT_ID',
+    apiKey: "AIzaSyDhEBotYsIo6H94OfGMENSh532CEKwoUVo",
+    authDomain: "gylde-sandbox.firebaseapp.com",
+    projectId: "gylde-sandbox",
+    storageBucket: "gylde-sandbox.firebasestorage.app",
+    messagingSenderId: "693709602499",
+    appId: "1:693709602499:web:ab7f6e65e178e889e5ec76",
+    measurementId: "G-B2YQNRZZNR"
   },
   firestoreLogLevel: 'info',
   googleMapsApiKey: 'AIzaSyAGjBhUWHSCD9Y0db3zP7ede1nxaGdYSXc',
